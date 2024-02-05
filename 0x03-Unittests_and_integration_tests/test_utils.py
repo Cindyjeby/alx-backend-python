043154686e299e677250998dd6fb47966448e9bd
cindyjeby <cindyjeby@gmail.com>
2024-02-05 17:35:52 +0000

    test 1

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -63,11 +63,11 @@ class TestGetJson(unittest.TestCase):
 
 
 class TestMemoize(unittest.TestCase):
-    """test the memoize function"""
+    """Tests the `memoize` function."""
     def test_memoize(self) -> None:
-        """tests memoize output"""
+        """Tests `memoize`'s output."""
         class TestClass:
-            def a_method (self):
+            def a_method(self):
                 return 42
 
             @memoize
